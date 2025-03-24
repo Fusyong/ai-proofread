60a8d295bccb3b7c445a15e5d052cb201832f94e
Huang Fusyong <aahuaang@163.com>
2025-03-24 15:03:43 +0800

    zip package back

--- vscode-extension/out/prompts.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=prompts.js.map
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+    // 外部化这些模块以减小打包体积
+    axios: 'commonjs axios',
+    dotenv: 'commonjs dotenv'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                module: 'es6'
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true
+  },
+  devtool: 'nosources-source-map',
+  mode: 'production'
+};
